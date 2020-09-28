d832f723648a436d7d4fa3fbb0a85ec62822795f
waha1206 <waha1206@gmail.com>
2020-09-28 11:31:18 +0800

    node-server

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express")
+const mongoose = require("mongoose")
+const app = express()
+
+const db = require("./config/keys").mongoURI
+
+app.get("/", (req,res)=>{
+	res.send("Hello World")
+})
+mongoose.connect(db
+,{useNewUrlParser: true,
+	useUnifiedTopology: true })
+.then(()=>console.log("MongoDB Connented"))
+.catch(err=>console.log(err))
+
+
+const port = process.env.PORT || 5000
+
+app.listen(port, ()=>{
+	console.log(`http://127.0.0.1:${port}`)
+})
