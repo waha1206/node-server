d7eac8064a4b43a0caf51744796dec35940fb0ef
waha1206 <waha1206@gmail.com>
2021-01-24 22:32:57 +0800

    備份

--- client/src/components/CategoriesManager/categoriesLevelThreeDialog.vue
+++ client/src/components/CategoriesManager/categoriesLevelThreeDialog.vue
@@ -7,134 +7,213 @@
       :close-on-press-escape="false"
       :modal-append-to-body="false"
       width="1200px"
-    >
-      <!-- :mmodel 綁定的會是 data return 裡面的某個物件 這邊綁定的是 levelOneData -->
-      <el-form
-        ref="levelThreeForm"
-        :model="levelThreeFormData"
-        :rules="form_rules"
-        label-width="120px"
-        style="margin:10px;width:auto"
-      >
-        <el-row :gutter="20" type="flex" class="row-bg">
-          <el-col :span="8"
-            ><div class="grid-content">
-              <el-form-item
-                label="先選擇大分類："
-                size="mini"
-                prop="levelOneId"
-                label-width="120px"
-              >
-                <!-- v-model 通常會指定到 formData (要提交的表單) 裡面的某個屬性 這邊是再 data return 裡面的 levelThreeData.levelOneId -->
-                <el-select
-                  v-model="levelThreeFormData.levelOneId"
-                  placeholder="請選擇"
-                  filterable
-                  @change="levelOneChang"
-                  size="mini"
-                >
-                  <!-- value 這邊綁定的是此 陣列裡面，要傳給 select v-mode 的值 -->
-                  <!-- label 就單純的顯示再 input 上面可以看到的文字 -->
-                  <el-option
-                    v-for="(item, index) in categoriesLevelOneData"
-                    :key="index"
-                    :label="item.name"
-                    :value="item._id"
-                  >
-                    <span style="float: left">{{ item.type }}</span>
-                    <span
-                      style="float: right; color: #8492a6; font-size: 13px"
-                      >{{ item.name }}</span
-                    >
-                  </el-option>
-                </el-select>
-              </el-form-item>
-            </div></el-col
-          >
-          <!-- 第二層的下拉選單 -->
-          <el-col :span="8"
-            ><div class="grid-content">
-              <el-form-item
-                label="選擇中分類："
-                size="mini"
-                prop="levelTwoId"
-                label-width="120px"
-              >
-                <el-select
-                  v-model="levelThreeFormData.levelTwoId"
-                  placeholder="請選擇"
-                  filterable
-                  @change="levelTwoChang"
-                  size="mini"
-                >
-                  <el-option
-                    v-for="(citem, cindex) in updateLevelTwoData"
-                    :key="cindex"
-                    :label="citem.name"
-                    :value="citem._id"
-                  >
-                    <span style="float: left">{{ citem.type }}</span>
-                    <span
-                      style="float: right; color: #8492a6; font-size: 13px"
-                      >{{ citem.name }}</span
-                    >
-                  </el-option>
-                </el-select>
-              </el-form-item>
-            </div></el-col
-          >
-          <!-- 輸入商品名稱 -->
-          <el-col :span="8">
-            <div class="grid-content">
-              <el-form-item
-                label="商品名稱："
-                size="mini"
-                label-width="120px"
-                prop="name"
-              >
-                <el-input
-                  type="name"
-                  v-model="levelThreeFormData.name"
-                ></el-input>
-              </el-form-item>
-            </div>
-          </el-col>
-        </el-row>
-        <!-- 取消與提交 -->
-        <el-row :gutter="20" type="flex" class="row-bg">
-          <el-col :span="12">
-            <div class="grid-content ">
-              <el-form-item
-                class="text_right"
-                label-width="110px"
-                style="margin-top:10px"
-              >
-                <el-button type="warning" @click="dialog.show = false"
-                  >取消</el-button
-                >
-                <el-button type="primary" @click="onSubmit('levelThreeForm')"
-                  >提交</el-button
-                >
-                <el-button @click="resetForm('levelThreeForm')">重置</el-button>
-              </el-form-item>
-            </div>
-          </el-col>
-        </el-row>
-      </el-form>
-      <!-- 表單結束 -->
+      ><el-container>
+        <!-- :mmodel 綁定的會是 data return 裡面的某個物件 這邊綁定的是 levelOneData -->
 
-      <!-- 要提交的formData 裡面的資料 -->
-      <p>要提交的表單內容：{{ levelThreeFormData }}</p>
-      <p>-----------------</p>
-      <p v-for="item in updateLevelTwoData">
-        {{ item.type }}{{ item.name }}--{{ item._id }}
-      </p>
-      <p>-----------------</p>
-      <!-- <p v-for="item in categoriesLevelTwoData">{{ item.name }}</p> -->
-      <!-- <p>{{ dialog }}</p>
+        <el-form
+          ref="levelThreeForm"
+          :model="levelThreeFormData"
+          :rules="form_rules"
+          label-width="120px"
+          style="margin:10px;width:auto"
+          ><el-header style="height: 80px;">
+            <el-row :gutter="20" type="flex" class="row-bg">
+              <el-col :span="8"
+                ><div class="grid-content">
+                  <el-form-item
+                    label="先選擇大分類："
+                    size="mini"
+                    prop="levelOneId"
+                    label-width="120px"
+                  >
+                    <!-- v-model 通常會指定到 formData (要提交的表單) 裡面的某個屬性 這邊是再 data return 裡面的 levelThreeData.levelOneId -->
+                    <el-select
+                      v-model="levelThreeFormData.levelOneId"
+                      placeholder="請選擇"
+                      filterable
+                      @change="levelOneChang"
+                      size="mini"
+                    >
+                      <!-- value 這邊綁定的是此 陣列裡面，要傳給 select v-mode 的值 -->
+                      <!-- label 就單純的顯示再 input 上面可以看到的文字 -->
+                      <el-option
+                        v-for="(item, index) in categoriesLevelOneData"
+                        :key="index"
+                        :label="item.name"
+                        :value="item._id"
+                      >
+                        <span style="float: left">{{ item.type }}</span>
+                        <span
+                          style="float: right; color: #8492a6; font-size: 13px"
+                          >{{ item.name }}</span
+                        >
+                      </el-option>
+                    </el-select>
+                  </el-form-item>
+                </div></el-col
+              >
+              <!-- 第二層的下拉選單 -->
+              <el-col :span="8"
+                ><div class="grid-content">
+                  <el-form-item
+                    label="選擇中分類："
+                    size="mini"
+                    prop="levelTwoId"
+                    label-width="120px"
+                  >
+                    <!-- 可複選的 select 要加入這三個屬性 allow-create default-first-option multiple -->
+                    <el-select
+                      v-model="levelThreeFormData.levelTwoId"
+                      placeholder="請選擇"
+                      filterable
+                      @change="levelTwoChang"
+                      size="mini"
+                    >
+                      <el-option
+                        v-for="(citem, cindex) in updateLevelTwoData"
+                        :key="citem._id"
+                        :label="citem.name"
+                        :value="citem._id"
+                      >
+                        <span style="float: left">{{ citem.type }}</span>
+                        <span
+                          style="float: right; color: #8492a6; font-size: 13px"
+                          >{{ citem.name }}</span
+                        >
+                      </el-option>
+                    </el-select>
+                  </el-form-item>
+                </div></el-col
+              >
+              <!-- 輸入商品名稱 -->
+              <el-col :span="8">
+                <div class="grid-content">
+                  <el-form-item
+                    label="商品名稱："
+                    size="mini"
+                    label-width="120px"
+                    prop="name"
+                  >
+                    <el-input
+                      type="name"
+                      v-model="levelThreeFormData.name"
+                    ></el-input>
+                  </el-form-item>
+                </div>
+              </el-col>
+            </el-row>
+
+            <!-- 第二行開始，備註說明 -->
+            <el-row :gutter="20" type="flex" class="row-bg">
+              <el-col :span="24">
+                <div class="grid-content">
+                  <el-form-item
+                    label="商品說明："
+                    size="mini"
+                    label-width="120px"
+                    prop="describe"
+                  >
+                    <el-input
+                      type="name"
+                      v-model="levelThreeFormData.describe"
+                    ></el-input>
+                  </el-form-item>
+                </div>
+              </el-col>
+            </el-row>
+          </el-header>
+          <!-- 第三行開始，圖片上傳 -->
+          <el-main>
+            <div class="image-warp">
+              <el-form-item
+                class="image-content"
+                label="圖片上傳："
+                size="mini"
+                label-width="120px"
+                prop="describe"
+              >
+                <el-upload
+                  action="#"
+                  list-type="picture-card"
+                  :auto-upload="false"
+                >
+                  <i slot="default" class="el-icon-plus"></i>
+                  <div class="image-content" slot="file" slot-scope="{ file }">
+                    <img
+                      class="el-upload-list__item-thumbnail"
+                      :src="file.url"
+                      alt=""
+                    />
+                    <span class="el-upload-list__item-actions">
+                      <span
+                        class="el-upload-list__item-preview"
+                        @click="handlePictureCardPreview(file)"
+                      >
+                        <i class="el-icon-zoom-in"></i>
+                      </span>
+                      <span
+                        v-if="!disabled"
+                        class="el-upload-list__item-delete"
+                        @click="handleDownload(file)"
+                      >
+                        <i class="el-icon-download"></i>
+                      </span>
+                      <span
+                        v-if="!disabled"
+                        class="el-upload-list__item-delete"
+                        @click="handleRemove(file)"
+                      >
+                        <i class="el-icon-delete"></i>
+                      </span>
+                    </span>
+                  </div>
+                </el-upload>
+              </el-form-item>
+              <!-- </el-col> -->
+              <!-- </el-col> -->
+            </div>
+          </el-main>
+          <!-- 取消與提交 -->
+          <el-row :gutter="20" type="flex" class="row-bg">
+            <el-col :span="24">
+              <div class="grid-content">
+                <el-form-item
+                  class="text_right"
+                  label-width="110px"
+                  style="margin-top:10px"
+                >
+                  <el-button type="warning" @click="dialog.show = false"
+                    >取消</el-button
+                  >
+                  <el-button type="primary" @click="onSubmit('levelThreeForm')"
+                    >提交</el-button
+                  >
+                  <el-button @click="resetForm('levelThreeForm')"
+                    >重置</el-button
+                  >
+                </el-form-item>
+              </div>
+            </el-col>
+          </el-row>
+        </el-form>
+        <!-- 表單結束 -->
+
+        <!-- 要提交的formData 裡面的資料 -->
+
+        <!-- <p>要提交的表單內容：{{ levelThreeFormData }}</p>
+        <p>-----------------</p>
+        <p v-for="item in updateLevelTwoData">
+          {{ item.type }}{{ item.name }}--{{ item._id }}
+        </p>
+        <p>-----------------</p>
+        <P>{{ dialogImageUrl }}</P> -->
+
+        <!-- <p v-for="item in categoriesLevelTwoData">{{ item.name }}</p> -->
+        <!-- <p>{{ dialog }}</p>
       <p>{{ formData }}</p>
       <p>{{ categoriesLevelOneData }}</p>
       -->
+      </el-container>
     </el-dialog>
   </div>
 </template>
@@ -150,11 +229,17 @@ export default {
   },
   data() {
     return {
+      // 圖片上傳
+      dialogImageUrl: '',
+      dialogVisible: false,
+      disabled: false,
       updateLevelTwoData: [],
+      // upload 第三層的商品 formData
       levelThreeFormData: {
         levelOneId: '',
         levelTwoId: '',
-        name: ''
+        name: '',
+        describe: ''
       },
       form_rules: {
         name: [{ required: true, message: '此欄位不能為空', trigger: 'blur' }]
@@ -167,6 +252,17 @@ export default {
     }
   },
   methods: {
+    // 圖片上傳的三個 methods
+    handleRemove(file) {
+      console.log(file)
+    },
+    handlePictureCardPreview(file) {
+      this.dialogImageUrl = file.url
+      this.dialogVisible = true
+    },
+    handleDownload(file) {
+      console.log(file)
+    },
     levelOneChang(id) {
       this.levelThreeFormData.levelTwoId = ''
       this.updateLevelTwoData = this.categoriesLevelTwoData.filter((item) => {
@@ -216,6 +312,7 @@ export default {
 <style scoped>
 .el-row {
   margin-bottom: 20px;
+
   &:last-child {
     margin-bottom: 0;
   }
@@ -236,8 +333,40 @@ export default {
   border-radius: 4px;
   min-height: 36px;
 }
+.el-main {
+  background-color: #e9eef3;
+  color: #333;
+  padding: 0;
+  margin: 0;
+  line-height: 400px;
+}
+.el-container:nth-child(7) .el-aside {
+  line-height: 320px;
+  width: 100%;
+}
 .row-bg {
-  padding: 10px 0;
+  padding: 0;
+  margin: 0;
+  height: 40px;
   background-color: #f9fafc;
+}
+
+.el-header,
+.el-footer {
+  background-color: #b3c0d1;
+  color: #333;
+  padding: 0;
+  margin: 0;
+  width: 1140px;
+  line-height: 40px;
+  height: 40px;
+}
+.image-warp {
+  margin-top: 15px;
+}
+
+.image-content {
+  float: left;
+  margin-bottom: 0px;
 }
 </style>
